3a8ff8a21f155770fc2ba275be6368d05499a315
Chub Sergey <chsv.sergey@gmail.com>
2023-02-21 14:54:37 +0200

    correct mobile-menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,19 +1,41 @@
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('[data-menu-open]'),
+//     closeMenuBtn: document.querySelector('[data-menu-close]'),
+//     menu: document.querySelector('[data-menu]'),
+//     body: document.querySelector('[body]'),
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleModal);
+//   refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.menu.classList.toggle('is-open');
+//   }
+//   function toggleModal() {
+//     refs.menu.classList.toggle('is-hidden');
+//     refs.body.classList.toggle('no-scroll');
+//   }
+// })();
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
-    body: document.querySelector('[body]'),
+    body: document.querySelector('body'),
+    menuList: document.querySelector('.mobile-menu-list'),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuList.addEventListener('click', removeMenu);
 
-  function toggleModal() {
-    refs.menu.classList.toggle('is-open');
-  }
-  function toggleModal() {
+  function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
+  }
+  function removeMenu() {
+    refs.menu.classList.add('is-hidden');
+    refs.body.classList.remove('no-scroll');
   }
 })();
